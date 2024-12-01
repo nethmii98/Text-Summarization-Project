ff49771af0aff8bb2f4aa834cae7c488925908e2
nethmii98 <nethminagodawithane777@gmail.com>
2024-12-01 11:54:34 +0530

    Model Training

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from textSummarization.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from textSummarization.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from textSummarization.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+from textSummarization.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
 from textSummarization.logging import logger
 
 STAGE_NAME = "Data Ingestion Stage"
@@ -36,3 +37,14 @@ except Exception as e:
     logger.exception(e)
     raise e
     
+
+STAGE_NAME = "Model Training Stage"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    model_trainer = ModelTrainerTrainingPipeline()
+    model_trainer.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx========x")
+except Exception as e:
+    logger.exception(e)
+    raise e    
