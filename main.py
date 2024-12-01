88d4d4f375e405bc9d1d5d4c0c22f8352e339e62
nethmii98 <nethminagodawithane777@gmail.com>
2024-12-01 17:58:42 +0530

    Model Evaluation

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from textSummarization.pipeline.stage_01_data_ingestion import DataIngestionTrai
 from textSummarization.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from textSummarization.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 from textSummarization.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
+from textSummarization.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
 from textSummarization.logging import logger
 
 STAGE_NAME = "Data Ingestion Stage"
@@ -44,6 +45,17 @@ try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     model_trainer = ModelTrainerTrainingPipeline()
     model_trainer.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx========x")
+except Exception as e:
+    logger.exception(e)
+    raise e    
+
+STAGE_NAME = "Model Evaluation Stage"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    model_evaluation = ModelEvaluationTrainingPipeline()
+    model_evaluation.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx========x")
 except Exception as e:
     logger.exception(e)
--- src/textSummarization/config/configuration.py
+++ src/textSummarization/config/configuration.py
@@ -1,6 +1,6 @@
 from textSummarization.constant import *
 from textSummarization.utils.common import read_yaml, create_directories
-from textSummarization.entity import DataIngestionConfig, DataValidationConfig,DataTransformationConfig, ModelTrainerConfig
+from textSummarization.entity import DataIngestionConfig, DataValidationConfig,DataTransformationConfig, ModelTrainerConfig, ModelEvaluationConfig
 
 class ConfigurationManager:
     def __init__(
@@ -74,4 +74,19 @@ class ConfigurationManager:
             gradient_accumulation_steps=params.gradient_accumulation_steps  
         )
 
-        return model_trainer_config     
+        return model_trainer_config   
+
+    def get_model_evaluation_config(self) -> ModelEvaluationConfig:
+        config = self.config.model_evaluation
+
+        create_directories([config.root_dir])
+        
+        model_evaluation_config = ModelEvaluationConfig(
+            root_dir=config.root_dir,
+            data_path=config.data_path,
+            model_path=config.model_path,
+            tokenizer_path=config.tokenizer_path,
+            metric_file_name=config.metric_file_name,
+        )
+
+        return model_evaluation_config     
